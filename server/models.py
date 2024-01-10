83672ccde116f7ed3b32c5c559906def575c3e9d
Maddie Weber <maddieweber7@gmail.com>
2024-01-09 20:25:56 -0500

    done

--- server/models.py
+++ server/models.py
@@ -19,6 +19,13 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    #relationship mapping employee to related reviews
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
+
+    #relationship mappig employee to related onboarding
+    onboarding = db.relationship('Onboarding', uselist=False, back_populates='employee')
+
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
 
@@ -30,6 +37,12 @@ class Onboarding(db.Model):
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
+
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
@@ -40,6 +53,12 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+
+    # Foreign key stores the Employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    #relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
